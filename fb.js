95342912c23c52ecc181d3c6333a920795a3d295
DivyaSolanki <divya1168@gmail.com>
2017-04-09 10:27:52 +0530

    change to local from heroku url

--- fb.js
+++ fb.js
@@ -2,5 +2,6 @@
 module.exports = {
 	'appID' : '1053444641423601',
 	'appSecret' : '7b18346d58e1e5caedcbb377b007a671',
-	'callbackUrl' : 'https://quiet-eyrie-89800.herokuapp.com/login/facebook/callback'
+	'callbackUrl' : 'http://localhost:3000'
+	//'callbackUrl' : 'https://quiet-eyrie-89800.herokuapp.com/login/facebook/callback'
 }
